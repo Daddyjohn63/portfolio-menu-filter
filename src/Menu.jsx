03da0a59747e70278104acae81cfbdc12eed94e1
Daddyjohn63 <john@webandprosper.co.uk>
2023-10-18 15:33:00 +0100

    final

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,12 +1,30 @@
 import MenuItem from './MenuItem';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const Menu = ({ items }) => {
   return (
     <div className="section-center">
-      {items.map(menuItem => {
-        return <MenuItem key={menuItem.id} {...menuItem} />;
-      })}
+      <TransitionGroup component={null}>
+        {items.map(menuItem => (
+          <CSSTransition key={menuItem.id} timeout={500} classNames="item">
+            <div>
+              <MenuItem {...menuItem} />
+            </div>
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 };
 export default Menu;
+
+// const Menu = ({ items }) => {
+//   return (
+//     <div className="section-center">
+//       {items.map(menuItem => {
+//         return <MenuItem key={menuItem.id} {...menuItem} />;
+//       })}
+//     </div>
+//   );
+// };
+// export default Menu;
